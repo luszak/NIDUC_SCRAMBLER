609d51661e968a1b6bb7c6df935fbcf65cb1d464
Lukasz Bieszczad <lukaszbieszczad0409@gmail.com>
2017-03-27 19:26:49 +0200

    DVB Scrambler simulation ver.1

--- Analizator.m
+++ Analizator.m
@@ -1,6 +1,7 @@
+function [a] = Analizator(vector1,vector2,vector3)
 n = size(vector1,2);
 tmp = 1;
-quantity = zeros(1,size(vector1,2)/5000);
+quantity = zeros(1,size(vector1,2));
 for i=1:n-1
     if vector1(i) == 0 && vector1(i+1)==0
         tmp = tmp + 1;
@@ -11,13 +12,61 @@ for i=1:n-1
         tmp = 1;
     end
 end
-% TODO
-for i=1:n
-   if quantity(i) == 0
-      quantity(i) = [];
-      i = i-1;
-      n = n-1;
+i = n;
+while(1)
+   if quantity(i) ~= 0
+       break;
    end
-plot(quantity, n);
+   quantity(i) = [];
+   i = i-1;
+end
+subplot(311)
+plot(quantity);
+n = size(vector2,2);
+tmp = 1;
+quantity = zeros(1,size(vector2,2));
+for i=1:n-1
+    if vector2(i) == 0 && vector2(i+1)==0
+        tmp = tmp + 1;
+    else
+        if vector2(i)== 0
+            quantity(tmp) = quantity(tmp) + 1;
+        end
+        tmp = 1;
+    end
+end
+i = n;
+while(1)
+   if quantity(i) ~= 0
+       break;
+   end
+   quantity(i) = [];
+   i = i-1;
+end
+subplot(312)
+plot(quantity);
+n = size(vector3,2);
+tmp = 1;
+quantity = zeros(1,size(vector3,2));
+for i=1:n-1
+    if vector3(i) == 0 && vector3(i+1)==0
+        tmp = tmp + 1;
+    else
+        if vector3(i)== 0
+            quantity(tmp) = quantity(tmp) + 1;
+        end
+        tmp = 1;
+    end
+end
+i = n;
+while(1)
+   if quantity(i) ~= 0
+       break;
+   end
+   quantity(i) = [];
+   i = i-1;
+end
+subplot(313)
+plot(quantity);
 end
 
---
+++ simulation1.m
@@ -0,0 +1,11 @@
+n = 10000;  % Liczba bitów do przetworzenia
+% Generacja danych
+zeros = zeroGenerator(n);
+ones = onesGenerator(n);
+random = randGenerator(n);
+% Scramblowanie
+zeros = DVBScrambler(zeros);
+ones = DVBScrambler(ones);
+random = DVBScrambler(random);
+%Analiza
+Analizator(zeros, ones, random);
